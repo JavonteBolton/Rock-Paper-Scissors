26a379fa29cf2ddffb0c0cb4274da444c7c207e7
Javonte Bolton <boltonjavonte@gmail.com>
2023-02-02 03:07:07 -0600

    Added Styling

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,41 +1,64 @@
-function getComputersMove() {
-    const computersChoices = Math.floor(Math.random() * 3)
-switch (computersChoices) {
-    case 0:
-        return "rock";
-    case 1:
-        return "paper";
-    case 2:
-        return "scissors";
-} 
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const playerChoiceDisplay = document.getElementById('player-choice');
+const resultDisplay = document.getElementById('result-display');
+
+const choices = document.querySelectorAll('button')
+
+let playerChoice
+let computerChoice
+let result
+
+
+choices.forEach(possibleChicken => possibleChicken.addEventListener('click', (e) => {
+    playerChoice = e.target.id
+    playerChoiceDisplay.innerHTML = playerChoice
+    generateCompChoice()
+    getResult()
+}))
+
+function generateCompChoice() {
+    const randomNum = Math.floor(Math.random() * 3) + 1
+
+if(randomNum === 1) {
+    computerChoice = 'rock'
+}
+if(randomNum === 2) {
+    computerChoice = 'paper'
+}
+if(randomNum === 3) {
+    computerChoice = 'scissors'
 }
 
-function playersChoices() {
-    const ask = prompt("can you beat the evil machine?").toLowerCase();
-    return ask;
+computerChoiceDisplay.innerHTML = computerChoice
+
 }
 
-function playRound(playerSelection, computerSelection) {
-
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "you win!"
-    } else if( playerSelection === "paper" && computerSelection === "rock") {
-        return "you win!"
-    } else if(playerSelection === "scissors" && computerSelection === "paper") {
-        return "you win!"
-    } else if (computerSelection === "scissors" && playerSelection === "paper") {
-        return "you loose"
-    } else if ( computerSelection === "paper" && playerSelection === "rock") {
-        return "you loose"
-    } else if (computerSelection === "rock" && playerSelection === "scissors") {
-        return "you loose"
-    } else if (playerSelection >= computerSelection) {
-        return "its a tie!"
-    } else if (computerSelection >= playerSelection) {
-        return "its a tie!"
-    }
+//if statements
+function getResult() {
+if(computerChoice === 'rock' && playerChoice === 'scissors') {
+    result = ' You Lose... :('
+}
+if(computerChoice === 'paper' && playerChoice === 'rock') {
+    result = ' You Lose... :('
+}
+if(computerChoice === 'scissors' && playerChoice === 'paper') {
+    result = ' You Lose... :('
+}
+if(playerChoice === 'rock' && computerChoice === 'scissors') {
+    result =' You Win! :)'
+}
+if(playerChoice === 'paper' && computerChoice === 'rock') {
+    result = ' You Win! :)'
+}
+if(playerChoice === 'scissors' && computerChoice === 'paper') {
+    result = ' You Win! :)'
+}
+if(playerChoice === computerChoice) {
+    result = ' Its A Draw :p'
 }
 
-    const playerSelection = playersChoices();
-    const computerSelection = getComputersMove();
-    console.log(playRound(playerSelection, computerSelection));
+resultDisplay.innerHTML = result
+}
+
+//results
+
